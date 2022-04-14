10ffcaa9a92290c79842544c5e9ece1f698c6b51
naymik-games <naymik@gmail.com>
2022-04-14 12:46:20 -0400

    test fix

--- scenes/preload.js
+++ scenes/preload.js
@@ -5,8 +5,71 @@ class preloadGame extends Phaser.Scene {
   preload() {
 
 
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', function (file) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+
+    this.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
 
     this.load.image("particle", "assets/sprites/particle.png");
+    for (var i = 0; i < 125; i++) {
+      this.load.image("particle", "assets/sprites/particle.png");
+    }
+
 
 
 
@@ -30,7 +93,7 @@ class preloadGame extends Phaser.Scene {
       frameHeight: 6
     });
 
-
+    this.load.image('lock', 'assets/sprites/lock.png');
     this.load.image('blank', 'assets/sprites/blank.png');
     this.load.image('back', 'assets/sprites/background.png');
     this.load.image('menu', 'assets/sprites/menu.png');
@@ -38,7 +101,7 @@ class preloadGame extends Phaser.Scene {
     this.load.image('view', 'assets/sprites/view.png');
     this.load.image('play', 'assets/sprites/play.png');
     this.load.image('coin', 'assets/sprites/coin.png');
-    this.load.image('lock', 'assets/sprites/lock.png');
+
 
   }
   create() {
