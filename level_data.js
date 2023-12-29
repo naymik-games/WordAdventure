4afa7ce3741fc2147e8bc4dbbadda3e6238293c6
naymik-games <naymik@gmail.com>
2023-12-28 23:28:30 -0500

    more levels

--- level_data.js
+++ level_data.js
@@ -3,7 +3,9 @@ groups = [
   { id: 1, title: 'Nature', puzzleCount: 5, levelStart: 5 },
   { id: 2, title: 'Places', puzzleCount: 5, levelStart: 10 },
   { id: 3, title: 'Star Wars', puzzleCount: 5, levelStart: 15 },
-  { id: 4, title: 'LOTR', puzzleCount: 5, levelStart: 20 }
+  { id: 4, title: 'LOTR', puzzleCount: 5, levelStart: 20 },
+  { id: 5, title: 'Computers', puzzleCount: 5, levelStart: 25 },
+  { id: 6, title: 'Animals', puzzleCount: 5, levelStart: 30 }
 
 ];
 //sizes
@@ -36,8 +38,29 @@ levels = [
 
   { level: 20, key: 'lotrh', theme: 'Hobbits', orient: 2, size: { cols: 8, rows: 10 }, overlap: true, words: ["bilbo", "frodo", "peregrin", "merry", "samwise", "lobelia", "proudfoot", "rose", "gaffer"] },
   { level: 21, key: 'lotrg', theme: 'Good', orient: 2, size: { cols: 8, rows: 10 }, overlap: true, words: ["gandolf", "treebeard", "galadriell", "elrond", "elfstone", "gimli", "bombadil", "smeagol"] },
-  { level: 22, key: 'lotrb', theme: 'Bad', orient: 2, size: { cols: 8, rows: 10 }, overlap: true, words: ["sauron", "saruman", "wormtounge", "witchking", "barlog", "orc", "gollum", "ted", "gorbag"] },
+  { level: 22, key: 'lotrb', theme: 'Bad', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["sauron", "saruman", "wormtounge", "witchking", "barlog", "orc", "gollum", "ted", "gorbag"] },
   { level: 23, key: 'lotrp', theme: 'Places', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["shire", "rivendell", "lorien", "mordor", "rohan", "minastirith", "moria", "helmsdeep", "isengard"] },
   { level: 24, key: 'lotrs', theme: 'Stuff', orient: 2, size: { cols: 8, rows: 10 }, overlap: true, words: ["aragorn", "andural", "butterbur", "weed", "ring", "havens", "elves", "bagend", "sharky", "partytree", "potato"] },
+
+  { level: 25, key: 'com1', theme: 'Hardware', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["keyboard", "mouse", "monitor", "harddrive", "printer", "server", "cable", "modem", "network"] },
+  { level: 26, key: 'com2', theme: 'Software', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["word", "excel", "outlook", "paint", "teams", "notepad", "chrome", "explorer"] },
+  { level: 27, key: 'com3', theme: 'Problems', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["crash", "virus", "phishing", "malware", "overheat", "whitescreen", "offline",] },
+  { level: 28, key: 'com4', theme: 'Coding', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["function", "editor", "git", "script", "code", "variable", "method", "python", "basic", "java"] },
+  { level: 29, key: 'com5', theme: 'Internet', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["wifi", "browser", "popup", "domain", "google", "bing", "online", "facebook", "twitter", "instagram"] },
+
+  { level: 30, key: 'animal1', theme: 'Dogs', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["chihuahua", "bulldog", "terrier", "collie", "shepherd", "boxer", "hound", "beagle", "rottweiler", "pinsher", "mastiff"] },
+  { level: 31, key: 'animal2', theme: 'Safari', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["lion", "leopard", "cheetah", "rhino", "elephant", "hippo", "giraffe", "hyena", "gazelle", "warthog", "monkey"] },
+  { level: 32, key: 'animal3', theme: 'Pets', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["dog", "cat", "hamster", "fish", "bird", "snake", "iguana", "ferret", "gerbil", "lizard", "turtle"] },
+  { level: 33, key: 'animal4', theme: 'On the Farm', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["cow", "goat", "pig", "chicken", "sheep", "turkey", "horse", "llama", "buffalo", "rooster"] },
+  { level: 34, key: 'animal5', theme: 'Kill You', orient: 2, size: { cols: 8, rows: 11 }, overlap: true, words: ["crocodile", "dingo", "shark", "cobra", "gator", "tarantula", "scorpian", "anaconda", "wolf", "stingray"] },
 ];
+
+/*
+
+
+
+
+
+
+*/
 
